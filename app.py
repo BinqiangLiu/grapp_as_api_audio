8b8f140a7bad13040b68af23c9ac82244b3a0947
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-01 23:39:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,9 @@ import uuid
 st.set_page_config(page_title="Gradio App as API WAudio", layout="wide")
 st.subheader("Gradio+Streamlit+WAudio+WtMem : Life Enhancing with AI!")
 
-result=""
+#result=""
+if "result" not in st.session_state:
+    st.session_state["result"] = ""
 
 css_file = "main.css"
 with open(css_file) as f:
@@ -36,14 +38,20 @@ with open(css_file) as f:
 
 hf_token=os.getenv('hf_token')
 
-user_query=st.text_input("Enter your query here: ")
+if "user_query" not in st.session_state:
+    st.session_state["user_query"] = ""
+#user_query=st.text_input("Enter your query here: ")
+st.session_state["user_query"]=st.text_input("Enter your query here: ")
 
 client = Client("https://binqiangliu-llama2-txt-gen.hf.space/")
-if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():
+#if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():
+if st.session_state["user_query"] !="" and not st.session_state["user_query"].strip().isspace() and not st.session_state["user_query"] == "" and not st.session_state["user_query"].strip() == "" and not st.session_state["user_query"].isspace():    
     with st.spinner("AI Thinking...Please wait a while to Cheers!"):
-        result = client.predict(user_query, api_name="/predict")
+        #result = client.predict(user_query, api_name="/predict")
+        st.session_state["result"] = client.predict(st.session_state["user_query"], api_name="/predict")
         st.write("AI Reponse: ")
-        st.write(result)
+        #st.write(result)
+        st.write(st.session_state["result"])
 
 translator = Translator()
 def text_to_speech(input_language, output_language, text):
@@ -64,14 +72,21 @@ def text_to_speech(input_language, output_language, text):
         return trans_text
 
 st.write("---")
-ai_response_audio = st.checkbox("语音播放AI助手回复", key="ai_audio_cbox")   
-if ai_response_audio:
+
+if "output_text" not in st.session_state:
+    st.session_state["output_text"] = ""
+
+if "ai_response_audio" not in st.session_state:
+    st.session_state["ai_response_audio"] = None
+#ai_response_audio = st.checkbox("语音播放AI助手回复", key="ai_audio_cbox")   
+st.session_state["ai_response_audio"] = st.checkbox("语音播放AI助手回复", key="ai_audio_cbox")   
+if st.session_state["ai_response_audio"]:
   out_lang = st.selectbox("请选择希望用来听AI回复的语言", ("English", "Chinese"), key="output_lang")
   if out_lang == "English":
     output_language = "en"
   elif out_lang == "Chinese":
     output_language = "zh-CN"
-  if result =="" or result.strip().isspace() or result == "" or result.strip() == ""  or result.isspace():
+  if st.session_state["result"] =="" or st.session_state["result"].strip().isspace() or st.session_state["result"] == "" or st.session_state["result"].strip() == ""  or st.session_state["result"].isspace():
     print("No AI Response Yet.")
     st.write("请确认您已经向AI助手提问并获得回复。")
   else:
@@ -80,4 +95,5 @@ if ai_response_audio:
         input_language_1 = "zh-CN"
     elif in_lang_1 == "English":
         input_language_1 = "en"    
-    output_text = text_to_speech(input_language_1, output_language, result)
+    #output_text = text_to_speech(input_language_1, output_language, st.session_state["result"])
+    st.session_state["output_text"] = text_to_speech(input_language_1, output_language, st.session_state["result"])
